a250178fc101a4255a0227a3aedcecb6701066d5
i7daydepressed <filonenko77500@gmail.com>
2026-09-19 18:59:00 +0400

    серое поправил - точка рефакторинга изображения теперь едина

--- Services/ImageProcessingService.cs
+++ Services/ImageProcessingService.cs
@@ -3,8 +3,31 @@ using ImageMagick;
 namespace ImageVisionApp.Services;
 
 public class ImageProcessingService{
-    public byte[] ConvertToGrayscale(byte[] sourceImageData)
-    {
+
+    public byte[] ApplyTransformations(
+        byte[] originalImageData,
+        bool isGrayscaleEnabled
+        ){
+        // каждый раз берем базовое избрж
+        using MagickImage processedImage = new MagickImage(originalImageData);
+
+        // пименяем только включенные преобразования
+        if (isGrayscaleEnabled){
+            // processedImage.Grayscale(PixelIntensityMethod.Rec709Luma);
+            this.ConvertToGrayscale(processedImage);
+        }
+        //тут
+        //тут и там
+        //и тут ещо
+
+        // возвращаем резулт
+        byte[] processedImageData = processedImage.ToByteArray(MagickFormat.Png);
+
+        return processedImageData;
+    }
+
+
+    public byte[] ConvertToGrayscale(byte[] sourceImageData){
         // из исходных байтов создаём объект изображения magick
         using MagickImage magickImage =
             new MagickImage(sourceImageData);
@@ -12,7 +35,7 @@ public class ImageProcessingService{
         // метод изменяет объект
         // в серое вот этим https://github.com/ImageMagick/ImageMagick/blob/fb965f1b54a65ddb633f8c2eac4452c782c66d7f/MagickCore/enhance.c#L2487-L2666
         magickImage.Grayscale(PixelIntensityMethod.Rec709Luma);
-
+            {
         // в нативном коде вариант Rec709Luma выбирает такую формулу чтобы rgb было умеренно серым в зависимости от текущ знач
         // intensity =
         //     0.212656 * red +
@@ -31,10 +54,38 @@ public class ImageProcessingService{
         //         blue = gray;
         //     }
         // }
-
+            }
         // назад в байты
         byte[] grayscaleImageData = magickImage.ToByteArray(MagickFormat.Png);
 
         return grayscaleImageData;
     }
+    public void ConvertToGrayscale(MagickImage processedImage){
+        // из исходных байтов создаём объект изображения magick
+        // метод изменяет объект
+        // в серое вот этим https://github.com/ImageMagick/ImageMagick/blob/fb965f1b54a65ddb633f8c2eac4452c782c66d7f/MagickCore/enhance.c#L2487-L2666
+        processedImage.Grayscale(PixelIntensityMethod.Rec709Luma);
+            {
+        // в нативном коде вариант Rec709Luma выбирает такую формулу чтобы rgb было умеренно серым в зависимости от текущ знач
+        // intensity =
+        //     0.212656 * red +
+        //     0.715158 * green +
+        //     0.072186 * blue;
+
+        // мега примерно что происходит
+        // for (int y = 0; y < imageHeight; y++){
+        //     for (int x = 0; x < imageWidth; x++){
+        //         double gray =
+        //             0.212656 * red +
+        //             0.715158 * green +
+        //             0.072186 * blue;
+        //         red = gray;
+        //         green = gray;
+        //         blue = gray;
+        //     }
+        // }
+            }
+    }
+
+
 }
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -30,7 +30,7 @@ public partial class MainViewModel : ViewModelBase{
     [ObservableProperty]
     public partial double BrightnessAdjustment { get; set; }= 0;
 
-    // 100 — исходная насыщенность изображения
+    // 100 — исходная насыщенность
     [ObservableProperty]
     public partial double SaturationPercentage { get; set; } = 100;
 
@@ -38,10 +38,19 @@ public partial class MainViewModel : ViewModelBase{
     [ObservableProperty]
     public partial double ContrastAdjustment { get; set; }= 0;
 
+    // false — изображение цветное
+    // true — применяются градации серого
+    [ObservableProperty]
+    public partial bool IsGrayscaleEnabled { get; set; } = false;
+
+
+
     public void LoadImage(byte[] imageData, string fileName){//реактим на событие загрузки збр
 
         // сохр исхд байты
         originalImageData = imageData;
+        // Новый файл открывается без преобразований предыдущего.
+        IsGrayscaleEnabled = false;// хз по идее надо джсончике параметры сохранять
         
         var newImageInfo =
                 imageStatsFileService.readImageInfo(imageData, fileName);
@@ -61,27 +70,41 @@ public partial class MainViewModel : ViewModelBase{
         StatusMessage = fileName;
     }
 
-    public void ConvertImageToGrayscale(){
+    private void updateModifiedImage() {
         if (originalImageData is null){
+            return;
+        }
+
+        // собираем результат заново с учетом всех настроек
+        byte[] processedImageData =
+            imageProcessingService.ApplyTransformations(//применяем фильтры
+                originalImageData,
+                IsGrayscaleEnabled);
+
+        using MemoryStream processedImageStream =
+            new MemoryStream(processedImageData);
+
+        // avalonia Bitmap принимает поток поэтому оборачиваем полученные байты в memstream
+        Bitmap newModifiedImage =
+            new Bitmap(processedImageStream);
+
+        ModifiedImage?.Dispose();
+
+        ModifiedImage = newModifiedImage;
+    }
+
+    public void ConvertImageToGrayscale() {
+        if (originalImageData is null) {
             StatusMessage = "Сначала выберите изображение";
             return;
         }
 
-        // originalImageData - данные до изменения ползунков - любое изменение ползунков будет браться относительно ориг даты
-
-        // наше серое изобрж
-        byte[] grayscaleImageData = imageProcessingService.ConvertToGrayscale(originalImageData);
-
-        // avalonia Bitmap принимает поток поэтому оборачиваем полученные байты в memstream
-        using MemoryStream grayscaleImageStream = new MemoryStream(grayscaleImageData);
-
-        Bitmap newModifiedImage = new Bitmap(grayscaleImageStream);
-
-        ModifiedImage?.Dispose();
-
-        // подставляем новое в вкладку
-        ModifiedImage = newModifiedImage;
+        // серое енаблед
+        IsGrayscaleEnabled = !IsGrayscaleEnabled;
+        // пересобираем
+        updateModifiedImage();
     }
+
 
     public void ShowError(string message)
     {
